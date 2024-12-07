a30981a8b3825f35f526c3c56a5e354cae5d08d5
mohintamboli03 <mohintamboli100@gmail.com>
2024-12-07 23:07:06 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -9,7 +9,7 @@ const AppContextProvider = (props) => {
   const [showLogin, setShowLogin] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [credit, setCredit] = useState(false);
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl ="https://imagify-1-zxax.onrender.com";
   const navigate=useNavigate() 
 
 
